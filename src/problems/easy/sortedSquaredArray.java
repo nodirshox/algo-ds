bde73dc1ed408504d4f89de12faa918e28da0ed7
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2022-05-08 22:25:50 -0500

    fix: variable names

--- src/problems/easy/sortedSquaredArray.java
+++ src/problems/easy/sortedSquaredArray.java
@@ -6,9 +6,9 @@ public class sortedSquaredArray {
     public static void main(String[] args) {
         System.out.println("Sorted Squared Array");
         // there is a bug when passed negative number
-        int[] input31 = { 1, 2, 3, 5, 6, 8, 9 };
-        int[] output31 = { 1, 4, 9, 25, 36, 64, 81 };
-        System.out.println(Arrays.equals(sortedSquaredArray(input31), output31));
+        int[] input = { 1, 2, 3, 5, 6, 8, 9 };
+        int[] output = { 1, 4, 9, 25, 36, 64, 81 };
+        System.out.println(Arrays.equals(sortedSquaredArray(input), output));
     }
 
     public static int[] sortedSquaredArray(int[] array) {
--- src/problems/easy/validateSequence.java
+++ src/problems/easy/validateSequence.java
@@ -3,9 +3,9 @@ package src.problems.easy;
 public class validateSequence {
     public static void main(String[] args) {
         System.out.println("Sorted Squared Array");
-        int[] input21 = { 5, 1, 22, 25, 6, -1, 8, 10 };
-        int[] input22 = { 1, 6, -1, 10 };
-        System.out.println(validateSubsequence(input21, input22) == true);
+        int[] input = { 5, 1, 22, 25, 6, -1, 8, 10 };
+        int[] input2 = { 1, 6, -1, 10 };
+        System.out.println(validateSubsequence(input, input2) == true);
     }
 
     public static boolean validateSubsequence(int[] array, int[] sequence) {
